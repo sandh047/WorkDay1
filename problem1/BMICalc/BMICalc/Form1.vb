489a51dd9988b4bc52ce643b89e721b1e0e899b8
idsc3101 <idsc3101@gmail.com>
2013-09-11 17:20:50 -0500

    Solved problem one.

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -4,5 +4,15 @@ Public Class Form1
 
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
 
+        Dim weight As Double    ' Stores the weight variable entered by the user
+        Dim height As Double    ' Stores the height variable entered by the user
+        Dim bmi As Double       ' Stores the bmi variable entered by the user
+
+        weight = CDbl(txtWeight.Text)               ' Converts the text in the weight text box into a double stored in the weight variable
+        height = CDbl(txtHeight.Text)               ' Converts the text in the height text box into a double stored in the weight variable
+        bmi = (weight * 703) / (height ^ 2)         ' Runs the formula for calculating bmi and stores it in the bmi variable
+        bmi = Math.Round(bmi, 2)                    ' Rounds the bmi variable to 2 decimal places
+        TextBox1.Text = Convert.ToString(bmi)       ' Displays the bmi variable in String format in the bmi text box
+
     End Sub
 End Class
